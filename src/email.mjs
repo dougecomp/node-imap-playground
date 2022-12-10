9cc818d5a0cf97787264c5d8cdc5cb50c2ac5b0c
Douglas Silva <douglaseusilva@gmail.com>
2022-12-10 16:41:54 -0300

    "tipagem" para os dados do e-mail

---
+++ src/email.mjs
@@ -0,0 +1,13 @@
+/* export type Email = {
+  id: string
+  from: {
+    name: string
+    address: string
+  }
+  to: {
+    name: string
+    address: string
+  }
+  subject: string
+  body: string
+} */
